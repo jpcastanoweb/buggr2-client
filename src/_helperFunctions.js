a83f900c838acbd8609bbbb9e17c28ca430f4eb9
Juan Pablo Castano <jpcastanom@gmail.com>
2021-08-16 22:42:26 -0500

    created helperFunctions file with toDateString

---
+++ src/_helperFunctions.js
@@ -0,0 +1,11 @@
+exports.toDateString = (dateS) => {
+  const date = new Date(dateS)
+  console.log(date)
+  return (
+    date.getFullYear() +
+    "-" +
+    (date.getMonth() > 8 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)) +
+    "-" +
+    (date.getDate() > 8 ? date.getDate() + 1 : "0" + (date.getDate() + 1))
+  )
+}
