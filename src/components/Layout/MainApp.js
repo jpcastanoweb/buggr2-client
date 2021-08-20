3a2e1c211e5a2e434817ba7227754ba8bef78028
Juan Pablo Castano <jpcastanom@gmail.com>
2021-08-20 09:24:41 -0500

    added styling to sidebar

--- src/components/Layout/MainApp.js
+++ src/components/Layout/MainApp.js
@@ -71,52 +71,95 @@ export default function MainApp(props) {
               />
               <span className="font-body text-3xl m-2 text-white">Buggr</span>
             </div>
-            <div className="mt-5 flex-1 flex flex-col">
+            <div className="mt-5 flex-1 flex flex-col ">
               <nav className="flex-1 space-y-1">
                 <Link
                   to="/app/customers"
                   className={
                     (activeTab === "customers"
-                      ? "border-r-4 border-green-400  "
+                      ? "border-r-4 border-green-400  flex flex-row justify-start items-center"
                       : "") +
-                    "px-10 text-indigo-100 hover:bg-purple-700 group flex items-center px-2 py-2 text-sm font-medium "
+                    "px-6 text-lg text-indigo-100 flex flex-row justify-start items-center hover:bg-purple-700 group flex items-center px-2 py-2 text-sm font-medium "
                   }
                   onClick={() => {
                     setActiveTab("customers")
                   }}
                 >
-                  Customers
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="white"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"
+                    />
+                  </svg>
+                  <span className="ml-4">Customers</span>
                 </Link>
 
                 <Link
                   to="/app/opportunities"
                   className={
-                    (activeTab === "opportunities"
-                      ? "border-r-4 border-green-400  "
+                    (activeTab === "customers"
+                      ? "border-r-4 border-green-400  flex flex-row justify-start items-center"
                       : "") +
-                    "px-10 text-indigo-100 hover:bg-purple-700 group flex items-center px-2 py-2 text-sm font-medium "
+                    "px-6 text-lg text-indigo-100 flex flex-row justify-start items-center hover:bg-purple-700 group flex items-center px-2 py-2 text-sm font-medium "
                   }
                   onClick={() => {
-                    setActiveTab("opportunities")
+                    setActiveTab("customers")
                   }}
                 >
-                  {/* <!-- Heroicon name: outline/folder --> */}
-                  Opportunities
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="white"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+
+                  <span className="ml-4">Opportunities</span>
                 </Link>
 
                 <Link
                   to="/app/projects"
                   className={
-                    (activeTab === "projects"
-                      ? "border-r-4 border-green-400  "
+                    (activeTab === "customers"
+                      ? "border-r-4 border-green-400  flex flex-row justify-start items-center"
                       : "") +
-                    "px-10 text-indigo-100 hover:bg-purple-700 group flex items-center px-2 py-2 text-sm font-medium "
+                    "px-6 text-lg text-indigo-100 flex flex-row justify-start items-center hover:bg-purple-700 group flex items-center px-2 py-2 text-sm font-medium "
                   }
                   onClick={() => {
-                    setActiveTab("projects")
+                    setActiveTab("customers")
                   }}
                 >
-                  Projects
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                    />
+                  </svg>
+
+                  <span className="ml-4">Projects</span>
                 </Link>
               </nav>
             </div>
